8460b92a9ae82c25e2cdb9e2a50c061d54aa2c17
kliko <kliko@softalix.com>
2023-12-28 18:13:54 +0200

    feat: price updates
    now every second price is updated and the client can get it through websocket

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const userController = require('./controllers/userController');
 
-router.use('/users', userController);
+router.use(userController);
 
 module.exports = router;
